3e370755630167d179a4e317a63b0eb7a903c924
lbryant-sss <brianmungai350@gmail.com>
2024-09-05 22:26:24 +0300

    Initial backend api

---
+++ backend/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from accounts.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('user/register', CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("api-auth/", include('rest_framework.urls')),
+]
